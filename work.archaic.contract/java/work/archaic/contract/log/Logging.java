52798b021cc91aae9308b703083e1dae5f96e29f
Henner <henner.schmidt@assense.com>
2024-12-28 18:20:30 +0100

    Add parameters to Logging Interface

--- work.archaic.contract/java/work/archaic/contract/log/Logging.java
+++ work.archaic.contract/java/work/archaic/contract/log/Logging.java
@@ -2,8 +2,8 @@ package work.archaic.contract.log;
 
 @SuppressWarnings("unused")
 public interface Logging {
-    void debug();
-    void info();
-    void warn();
-    void error();
-}
+    void debug(Object... args);
+    void info(Object... args);
+    void warn(Object... args);
+    void error(Object... args);
+    }
