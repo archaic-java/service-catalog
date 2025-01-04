c99d0a0231795c3c4a190584cfc74b483c911836
Henner <henner.schmidt@assense.com>
2025-01-04 16:24:29 +0100

    Remove optionality of dependency to logger

--- work.archaic.contract/java/module-info.java
+++ work.archaic.contract/java/module-info.java
@@ -1,4 +1,5 @@
 module work.archaic.contract {
+    uses Logging_02;
     exports work.archaic.contract.log;
     exports work.archaic.contract.test;
 }
---
+++ work.archaic.contract/java/work/archaic/contract/log/Logging_02.java
@@ -0,0 +1,10 @@
+package work.archaic.contract.log;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ServiceLoader;
+
+public interface Logging_02 {
+
+}
