d2bb23e2743da8c6e27c7bcf1ccd51daf1302688
Henner <henner.schmidt@assense.com>
2025-01-05 14:16:21 +0100

    Introduce new Logging contract and implementation

--- work.archaic.contract/java/work/archaic/contract/log/v02/Logging.java
+++ work.archaic.contract/java/work/archaic/contract/log/v02/Logging.java
@@ -1,5 +1,25 @@
 package work.archaic.contract.log.v02;
 
 public interface Logging {
+    /**
+     * @param message describing current step on a trail. Will only be logged in case of a later anomaly
+     *                or bug.
+     */
+    void trail(String message);
 
+    /**
+     * @param message describing an event outside a trail. Example: "Application initialization finished."
+     */
+    void event(String message);
+
+    /**
+     * @param exception describing circumstances that are not "normal" but do not lead to service degradation.
+     *                Example: API XY could not be reached but can be retried
+     */
+    void anomaly(Exception exception);
+
+    /**
+     * @param throwable being an unexpected outcome.
+     */
+    void bug(Throwable throwable);
 }
