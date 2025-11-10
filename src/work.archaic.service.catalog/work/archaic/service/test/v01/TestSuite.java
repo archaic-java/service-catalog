d931b6342cd5a251b66c2bb1b8ee105292adf6aa
mitschwimmer <henner@mitschwimmer.de>
2025-11-10 20:27:25 +0100

    Make usage of setup() in test suites clearer

--- src/work.archaic.service.catalog/work/archaic/service/test/v01/TestSuite.java
+++ src/work.archaic.service.catalog/work/archaic/service/test/v01/TestSuite.java
@@ -6,9 +6,9 @@ package work.archaic.service.test.v01;
  * <p>Defines the contract for test suites with optional lifecycle hooks.
  */
 public interface TestSuite {
-  /** Optional hook invoked once before any @Test in this suite. */
+  /** Optional hook invoked once per @TestSuite. */
   default void setup() throws Exception {}
 
-  /** Optional hook invoked once after all @Test in this suite. */
+  /** Optional hook invoked once after all @Test of this suite have been run. */
   default void teardown() throws Exception {}
 }
